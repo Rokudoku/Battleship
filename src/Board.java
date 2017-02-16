8ae874efde3a7aba5895e3130dec18960deca231
Rokudoku <Rokudoku@users.noreply.github.com>
2017-02-16 12:46:46 +1100

    Board height and width are now constants. Added isValidTile method.

--- src/Board.java
+++ src/Board.java
@@ -23,13 +23,13 @@ public class Board {
 	public final static boolean EMPTY = false;
 	public final static boolean SHIP  = true;
 	
-	protected static int boardHeight = 10;
-	protected static int boardWidth = 10;
+	public final static int BOARD_HEIGHT = 10;
+	public final static int BOARD_WIDTH = 10;
 	
-	protected boolean[][] board = new boolean[boardHeight][boardWidth];
+	protected boolean[][] board = new boolean[BOARD_HEIGHT][BOARD_WIDTH];
 	
 	public Board() {
-		for (int row = 0; row < boardHeight; row++) {
+		for (int row = 0; row < BOARD_HEIGHT; row++) {
 			Arrays.fill(board[row], EMPTY);
 		}
 	}
@@ -38,5 +38,10 @@ public class Board {
 		String rowLetters[] = { "A", "B", "C", "D", "E",
 								"F", "G", "H", "I", "J" };
 		return rowLetters[rowNumber];
+	}
+	
+	public boolean isValidTile(int row, int column) {
+		return row >= 0 && row < BOARD_HEIGHT 
+				&& column >= 0 && column < BOARD_WIDTH;
 	}
 }
--- src/BoardEnemy.java
+++ src/BoardEnemy.java
@@ -12,9 +12,9 @@ public class BoardEnemy extends Board{
 		System.out.println("  | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10|");
 		System.out.println("  -----------------------------------------");
 		
-		for (int row = 0; row < boardHeight; row++) {
+		for (int row = 0; row < BOARD_HEIGHT; row++) {
 			System.out.print(this.getRowLetter(row) + " |");
-			for (int column = 0; column < boardWidth; column++) {
+			for (int column = 0; column < BOARD_WIDTH; column++) {
 				if (board[row][column] == EMPTY) {
 					System.out.print(" -  ");
 				} else {
