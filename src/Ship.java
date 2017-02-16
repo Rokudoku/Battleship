985b4ac673feb8ba6d593ba4a2bcc5bb72f9ec89
Rokudoku <Rokudoku@users.noreply.github.com>
2017-02-16 14:36:57 +1100

    Ships randomly get assigned RIGHT or DOWN.

--- src/Ship.java
+++ src/Ship.java
@@ -4,17 +4,25 @@
  * Ships start at a single x/y origin and go either down or right.
  */
 public class Ship {
-	public final static int RIGHT = 1;
-	public final static int DOWN = 2;
+	public final static int RIGHT = 0;
+	public final static int DOWN = 1;
+	public final static int NUM_DIRECTIONS = 2;
 	
 	private final int xOrigin;
 	private final int yOrigin;
 	private final int size;
+	private int direction;
 
 	public Ship(int xOrigin, int yOrigin, int size) {
 		this.xOrigin = xOrigin;
 		this.yOrigin = yOrigin;
 		this.size = size;
+		// assign either RIGHT or DOWN
+		direction = (int) Math.random() * NUM_DIRECTIONS;
+	}
+	
+	public int getDirection() {
+		return direction;
 	}
 	
 	public int getX() {
