f50b6033469607a12d69f3b15dfef62f48f8b5a0
Rokudoku <Rokudoku@users.noreply.github.com>
2017-02-18 18:20:33 +1100

    Added and tested isValidInput

--- src/Game.java
+++ src/Game.java
@@ -10,5 +10,17 @@ public class Game {
 		boardComputer.reduceHitPoints(Board.DESTROYER);
 		boardComputer.printRadar();
 		boardComputer.testHP();
+		System.out.println(isValidInput("E4"));
+		System.out.println(isValidInput("B10"));
+		System.out.println(isValidInput("B11"));
+		System.out.println(isValidInput("MM"));
+		System.out.println(isValidInput("M8"));
+		System.out.println(isValidInput("4E"));
+		System.out.println(isValidInput("88"));
+		System.out.println(isValidInput("J1"));
+	}
+	
+	private static boolean isValidInput(String input) {
+		return input.matches("^[A-Ja-j][1-9]$") || input.matches("[A-Ja-j]10");
 	}
 }
